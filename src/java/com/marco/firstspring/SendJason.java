f21f71a163c093f080bc06cf12ed8220966f5e61
mhauff <marco.hauff@epay.ingenico.com>
2019-08-09 15:39:06 +0200

    2nd brnache 3rd commit

---
+++ src/java/com/marco/firstspring/SendJason.java
@@ -0,0 +1,62 @@
+package com.marco.firstspring;
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+
+
+public class SendJason {
+    public static void main(String[] args) {
+        SendJason.Post_JSON();
+    }
+    public static void Post_JSON() {
+        String query_url = "https://int-travelhub.gcsip.nl/travelhub/api/tpa/v1/paymentMethods";
+        //String json = "{ \"method\" : \"guru.test\", \"params\" : [ \"jinu awad\" ], \"id\" : 123 }";
+        //String json = "{ \"method\" : \"guru.test\", \"params\" : [ \"Marco\" ], \"id\" : 123 }";
+
+        // header info needed
+
+       // "headers": {
+       //     "Accept": "application/json",
+       //             "Content-Type": "application/json",
+       //             "Authorization": "Basic MjZhZjZiMTdjZTE5NWIwZDozMjFjNzc2M2Q0MzAwNTk1"
+
+
+        String json = "{\n  \"merchantId\" : \"23456780\"\n}";
+
+       // con.setRequestProperty( "Authorization", "Basic " + encoder.encode( encodedPassword ) )
+
+        System.out.println(json);
+
+        try {
+            URL url = new URL(query_url);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setConnectTimeout(5000);
+            conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty( "Authorization", "Basic MjZhZjZiMTdjZTE5NWIwZDozMjFjNzc2M2Q0MzAwNTk1" );
+            OutputStream os = conn.getOutputStream();
+            os.write(json.getBytes("UTF-8"));
+            os.close();
+            // read the response
+            InputStream in = new BufferedInputStream(conn.getInputStream());
+            String result = IOUtils.toString(in, "UTF-8");
+            System.out.println(result);
+            System.out.println("result after Reading JSON Response");
+            JSONObject myResponse = new JSONObject(result);
+            System.out.println("jsonrpc- "+myResponse.getString("jsonrpc"));
+            System.out.println("id- "+myResponse.getInt("id"));
+            System.out.println("result- "+myResponse.getString("result"));
+            in.close();
+            conn.disconnect();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
